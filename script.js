d58eac8cab387290489db8500fde3742ffc3bd50
Sergii Nikolayev <nikolayev.sergii@gmail.com>
2018-01-23 14:35:35 +0200

    2 my changes

--- script.js
+++ script.js
@@ -5,6 +5,57 @@ var questionsList = [{
     hint: "heard",
     question: {
         beforeInput: "I ",
+        afterInput: "11111 (hear) a new song on the radio."
+    },
+    answers: ["heard", "heard1", "heard2"]
+}, {
+    name: "read",
+    hint: "read",
+    question: {
+        beforeInput: "I ",
+        afterInput: " (read) three books last week."
+    },
+    answers: ["read", "read1", "read2"]
+}];
+var questionsList2 = [{
+    name: "001222",
+    hint: "heard222",
+    question: {
+        beforeInput: "I ",
+        afterInput: "222222 (hear) a new song on the radio."
+    },
+    answers: ["heard", "heard1", "heard2"]
+}, {
+    name: "read",
+    hint: "read",
+    question: {
+        beforeInput: "I ",
+        afterInput: " (read) three books last week."
+    },
+    answers: ["read", "read1", "read2"]
+}];
+var questionsList3 = [{
+    name: "001dsfs",
+    hint: "333heardsdfsfs",
+    question: {
+        beforeInput: "I ",
+        afterInput: " 33333(hear) a new song on the radio."
+    },
+    answers: ["heard", "heard1", "heard2"]
+}, {
+    name: "333333read",
+    hint: "3333read",
+    question: {
+        beforeInput: "I ",
+        afterInput: " (read) three books last week."
+    },
+    answers: ["read", "read1", "read2"]
+}];
+var questionsList4 = [{
+    name: "001222",
+    hint: "heard222",
+    question: {
+        beforeInput: "I ",
         afterInput: " (hear) a new song on the radio."
     },
     answers: ["heard", "heard1", "heard2"]
@@ -16,11 +67,22 @@ var questionsList = [{
         afterInput: " (read) three books last week."
     },
     answers: ["read", "read1", "read2"]
-}]
+}];
 
-var QuizModule = function() {
+
+var qWrapper = document.getElementById("questionsWrapper");
+var qWrapper2 = document.getElementById("questionsWrapper2");
+var qWrapper3 = document.getElementById("questionsWrapper3");
+var qWrapper4 = document.getElementById("questionsWrapper4");
+
+
+/**
+ * htmlWraper - or html id, where to add content
+ *
+ * */
+
+var QuizModule = function(htmlWrapper, listOfQuestions) {
     var questions = [];
-    var qWrapper = document.getElementById("questionsWrapper");
 
     function init(questionsArray) {
         //adding questions to our inner Storage;
@@ -40,7 +102,7 @@ var QuizModule = function() {
         console.log(questionsArray);
         if (length > 0) {
             for (var i = 0; i < length; i++) {
-                //console.log(questionsArray[i]);
+                console.log(questionsArray[i]);
                 var content = "<tr>" +
                     "<td>" +
                     "<div>" +
@@ -51,10 +113,11 @@ var QuizModule = function() {
                     "<td>" +
                     "<button id=" + questionsArray[i].name + "-check" + ">Проверить</button><button id=" + questionsArray[i].hint + "-hint" + ">Подсказка</button>" +
                     "</td>" +
-                    "</tr>"
-                qWrapper.innerHTML += content;
+                    "</tr>";
+                htmlWrapper.innerHTML += content;
             }
         }
+
     }
 
     function check(id, answer) {
@@ -69,17 +132,27 @@ var QuizModule = function() {
     function hint(id) {
         console.log("hint: ", id);
     }
+
     return {
         init: init,
         check: check,
         hint: hint
     }
-}
+};
 
-var questionHandler = new QuizModule();
+var questionHandler = new QuizModule(qWrapper);
 questionHandler.init(questionsList);
+
+var questionHandler = new QuizModule(qWrapper2,questionsList2);
+questionHandler.init(questionsList2);
+
+var questionHandler = new QuizModule(qWrapper3);
+questionHandler.init(questionsList3);
+
+
+
 document.addEventListener('click', function(e) {
-    console.log(e)
+    console.log(e);
     if (e.target.id.indexOf("hint") !== -1) {
         questionHandler.hint(e.target.id)
     } else if (e.target.id.indexOf("check") !== -1) {
@@ -88,4 +161,4 @@ document.addEventListener('click', function(e) {
     } else {
         console.log("some error appears when Question was creating check question with name: ", e.target.id.slice(0, e.target.id.indexOf("-")));
     }
-})
+});
