9363bc019954b347bc5f3c559fd00b36eb586005
Sergii Nikolayev <nikolayev.sergii@gmail.com>
2018-01-24 14:16:02 +0200

    some colors, multy DB added

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 console.clear();
 //we received this array from DB
+/**
+ * Our DataBases
+ * */
 var iAme;
 var heIsNot = ["he is not", "he isn't", "he's not"];
 var sheIsNot = ["she is not", "she isn't", "she's not"];
@@ -9,7 +12,7 @@ var questionsList = [{
     hint: "heard",
     question: {
         beforeInput: "I ",
-        afterInput: "11111 (hear) a new song on the radio."
+        afterInput: "1 (hear) a new song on the radio."
     },
     answers: heIsNot//["heard", "heard1", "heard2", "he is not", "he isn't"]
 }, {
@@ -22,14 +25,61 @@ var questionsList = [{
     answers: ["read", "read1", "read2"]
 }];
 
-var qWrapper = document.getElementById("questionsWrapper");
+var questionsList2 = [{
+    name: "021",
+    hint: "is",
+    question: {
+        beforeInput: "She ",
+        afterInput: " a nice woman"
+    },
+    answers: ["is", "'s"]
+}, {
+    name: "022",
+    hint: "are",
+    question: {
+        beforeInput: "We ",
+        afterInput: " at work"
+    },
+    answers: ["are", "'re"]
 
+}];
+
+
+var questionsList3 = [{
+    name: "031",
+    hint: "3heard",
+    question: {
+        beforeInput: "I ",
+        afterInput: "3 (hear) a new song on the radio."
+    },
+    answers: ["3heard", "3heard1", "3heard2"]
+}, {
+    name: "032",
+    hint: "3read",
+    question: {
+        beforeInput: "I ",
+        afterInput: " (read) three books last week."
+    },
+    answers: ["3read", "3read1", "3read2"]
+}];
+
+/**
+ * HTML id's
+ * */
+var qWrapper = document.getElementById("questionsWrapper");
+var qWrapper2 = document.getElementById("questionsWrapper2");
+var qWrapper3 = document.getElementById("questionsWrapper3");
+
+
+/**
+ * Main Module
+ * */
 /**
  * htmlWraper - id, where to add content
  *
  * */
 
-var QuizModule = function(htmlWrapper, listOfQuestions) {
+var QuizModule = function (htmlWrapper, listOfQuestions) {
     var questions = [];
 
     function init(questionsArray) {
@@ -71,23 +121,31 @@ var QuizModule = function(htmlWrapper, listOfQuestions) {
 
     function check(id, userAnswer) {
         //console.log("check: ", userAnswer + " , " + id);
-        var questionId = id.slice(0,-6); //cutting up  "-check"
+        var questionId = id.slice(0, -6); //cutting up  "-check"
         //console.log(questionId);
         var length = questions.length;
+        var button = document.getElementById(id);
         for (var i = 0; i < length; i++) {
-          if (questions[i].name == questionId){
-            //console.log(questions[i].answers);
-            questions[i].answers.forEach(function(element){
-                if (userAnswer == element){
-                    console.log("correct " + element);
-                } else if (userAnswer === ""){
-                    console.log("type your answer");       
-                } else if (userAnswer !== element){
-                    console.log("not correct " + element);
-                }
-            });
-          }
-          
+            if (questions[i].name == questionId) {
+                //console.log(questions[i].answers);
+                questions[i].answers.forEach(function (element) {
+                    if (userAnswer == "" || userAnswer == " "){
+                        console.log("type your answer");
+                        return button.innerHTML = "Введите слово";
+                    } else if (userAnswer !== element) {
+                        console.log("not correct " + element);
+                        button.style.backgroundColor = "#ff4d4d";
+                        button.style.color = "white";
+                        return button.innerHTML = "Еще раз";
+                    } else if (userAnswer == element){
+                        console.log("correct " + element);
+                        button.style.backgroundColor = "green";
+                        button.style.color = "white";
+                        return button.innerHTML = "Правильно";
+                    }
+                });
+            }
+
 
         }
     }
@@ -103,17 +161,58 @@ var QuizModule = function(htmlWrapper, listOfQuestions) {
     }
 };
 
+
+/**
+ *
+ * questionHandler's
+ *
+ * */
+
 var questionHandler = new QuizModule(qWrapper);
 questionHandler.init(questionsList);
-
-document.addEventListener('click', function(e) {
+document.addEventListener('click', function (e) {
     console.log(e);
     if (e.target.id.indexOf("hint") !== -1) {
         questionHandler.hint(e.target.id)
     } else if (e.target.id.indexOf("check") !== -1) {
         var input = document.getElementById(e.target.id.slice(0, e.target.id.indexOf("-")));
         var userInputNoSpace = input.value.replace(/^\s+|\s+$/g, ""); //.replace(/^\s+|\s+$/g, "") - cutting all spaces before and after input.value
-        questionHandler.check(e.target.id, userInputNoSpace)  
+        questionHandler.check(e.target.id, userInputNoSpace)
+    } else {
+        console.log("some error appears when Question was creating check question with name: ", e.target.id.slice(0, e.target.id.indexOf("-")));
+    }
+});
+
+
+
+var questionHandler2 = new QuizModule(qWrapper2);
+questionHandler2.init(questionsList2);
+document.addEventListener('click', function (e) {
+    console.log(e);
+    if (e.target.id.indexOf("hint") !== -1) {
+        questionHandler2.hint(e.target.id)
+    } else if (e.target.id.indexOf("check") !== -1) {
+        var input = document.getElementById(e.target.id.slice(0, e.target.id.indexOf("-")));
+        var userInputNoSpace = input.value.replace(/^\s+|\s+$/g, ""); //.replace(/^\s+|\s+$/g, "") - cutting all spaces before and after input.value
+        questionHandler2.check(e.target.id, userInputNoSpace)
+    } else {
+        console.log("some error appears when Question was creating check question with name: ", e.target.id.slice(0, e.target.id.indexOf("-")));
+    }
+});
+
+
+
+
+var questionHandler3 = new QuizModule(qWrapper3);
+questionHandler3.init(questionsList3);
+document.addEventListener('click', function (e) {
+    console.log(e);
+    if (e.target.id.indexOf("hint") !== -1) {
+        questionHandler3.hint(e.target.id)
+    } else if (e.target.id.indexOf("check") !== -1) {
+        var input = document.getElementById(e.target.id.slice(0, e.target.id.indexOf("-")));
+        var userInputNoSpace = input.value.replace(/^\s+|\s+$/g, ""); //.replace(/^\s+|\s+$/g, "") - cutting all spaces before and after input.value
+        questionHandler3.check(e.target.id, userInputNoSpace)
     } else {
         console.log("some error appears when Question was creating check question with name: ", e.target.id.slice(0, e.target.id.indexOf("-")));
     }
